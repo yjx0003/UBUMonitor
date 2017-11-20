24f750d3147004a1aea3bad984d2cd29338c5403
Felix Nogal <fns0002@alu.ubu.es>
2017-11-20 18:12:02 +0100

    Modificada la exportación de los gráficos.

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,6 +1,8 @@
 package controllers;
 
 import java.awt.Desktop;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -12,6 +14,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
 
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.slf4j.Logger;
@@ -19,7 +22,6 @@ import org.slf4j.LoggerFactory;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -29,7 +31,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.SnapshotParameters;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
@@ -43,7 +44,6 @@ import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.image.WritableImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
@@ -637,9 +637,7 @@ public class MainController implements Initializable {
 	 * @param actionEvent
 	 * @throws Exception
 	 */
-	public void saveChart(ActionEvent actionEvent) throws Exception {
-		WritableImage image = webViewCharts.snapshot(new SnapshotParameters(), null);
-		
+	public void saveChart(ActionEvent actionEvent) throws Exception {		
 		File file = new File("chart.png");
 
 		FileChooser fileChooser = new FileChooser();
@@ -653,7 +651,10 @@ public class MainController implements Initializable {
 			file = fileChooser.showSaveDialog(UBUGrades.stage);
 			if (file != null) {
 				try {
-					ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+					String str = (String) webViewChartsEngine.executeScript("exportCurrentElemet()");
+					byte[] imgdata = DatatypeConverter.parseBase64Binary(str.substring(str.indexOf(",") + 1));
+					BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imgdata));
+					ImageIO.write(bufferedImage, "png", file);
 				} catch (IOException ex) {
 					logger.info(ex.getMessage());
 				}

cb65b321679e91b208ca48febb1f406b413be649
yjx0003 <yipengji@gmail.com>
2019-03-18 22:21:29 +0100

    Creado todas las clases disponibles en moodle de actividades y recursos #9

---
+++ src/model/mod/Label.java
@@ -0,0 +1,10 @@
+package model.mod;
+
+public class Label extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/model/mod/ModuleType.java
@@ -0,0 +1,159 @@
+package model.mod;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ModuleType {
+
+	ASSIGNMENT("assign") {
+		@Override
+		public Module create() {
+			return new Assignment();
+		}
+	},
+	CHAT("chat") {
+		@Override
+		protected Module create() {
+			return new Chat();
+		}
+	},
+	CHOICE("choice") {
+		@Override
+		public Module create() {
+			return new Choice();
+		}
+	},
+	DATABASE("data") {
+		@Override
+		protected Module create() {
+			return new Database();
+		}
+	},
+	EXTERNAL_TOOL("lti") {
+		@Override
+		protected Module create() {
+			return new ExternalTool();
+		}
+	},
+	FEEDBACK("feedback") {
+		@Override
+		protected Module create() {
+			return new Feedback();
+		}
+	},
+	FILE("resource"){
+		@Override
+		protected Module create() {
+			return new File();
+		}
+	},
+	FOLDER("folder") {
+		@Override
+		public Module create() {
+			return new Folder();
+		}
+	},
+	FORUM("forum") {
+		@Override
+		public Module create() {
+			return new Forum();
+		}
+	},
+	
+	IMS_PACKAGE("imscp"){
+		@Override
+		protected Module create() {
+			return new IMSContentPackage();
+		}
+	},
+	GLOSSARY("glossary") {
+		@Override
+		protected Module create() {
+			return new Glossary();
+		}
+	},
+	LABEL("label") {
+		@Override
+		public Module create() {
+			return new Label();
+		}
+	},
+	LESSON("lesson") {
+		@Override
+		public Module create() {
+			return new Lesson();
+		}
+	},
+	MODULE("module") {
+		@Override
+		public Module create() {
+			return new Module();
+		}
+	},
+	PAGE("page") {
+		@Override
+		public Module create() {
+			return new Page();
+		}
+	},
+	QUIZ("quiz") {
+		@Override
+		public Module create() {
+			return new Quiz();
+		}
+	},
+	SCORM_PACKAGE("lti"){
+		@Override
+		protected Module create() {
+			return new SCORMPackage();
+		}
+	},
+	SURVEY("survey") {
+		@Override
+		protected Module create() {
+			return new Survey();
+		}
+	},
+	URL("url") {
+		@Override
+		public Module create() {
+			return new URL();
+		}
+	},
+	WIKI("wiki") {
+		@Override
+		protected Module create() {
+			return new Wiki();
+		}
+	},
+	WORKSHOP("workshop") {
+		@Override
+		public Module create() {
+			return new Workshop();
+		}
+	};
+
+	private String modname;
+	private static Map<String, ModuleType> modTypes;
+
+	ModuleType(String modname) {
+		this.modname = modname;
+	}
+
+	static {
+		modTypes = new HashMap<String, ModuleType>();
+		for (ModuleType mod : ModuleType.values()) {
+			modTypes.put(mod.modname, mod);
+		}
+	}
+
+	protected abstract Module create();
+
+	public static ModuleType get(String modname) {
+		return modTypes.get(modname);
+	}
+
+	public static Module createInstance(String modname) {
+		return modTypes.getOrDefault(modname, MODULE).create();
+	}
+}

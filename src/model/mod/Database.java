c1238ad8c783d51a0229ecf16eb85aec4c2cf79e
yjx0003 <yipengji@gmail.com>
2019-03-18 22:24:41 +0100

    Añadido mas elementos de moodle #9

---
+++ src/model/mod/Database.java
@@ -0,0 +1,10 @@
+package model.mod;
+
+public class Database extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/model/mod/File.java
@@ -0,0 +1,10 @@
+package model.mod;
+
+public class File extends Module{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}

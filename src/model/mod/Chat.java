a6c7de14dd091b5ca20c434e05370c6c956005f1
yjx0003 <yipengji@gmail.com>
2019-03-18 22:24:41 +0100

    Añadido mas elementos de moodle #9

---
+++ src/model/mod/Chat.java
@@ -0,0 +1,10 @@
+package model.mod;
+
+public class Chat extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ src/model/mod/ExternalTool.java
@@ -0,0 +1,10 @@
+package model.mod;
+
+public class ExternalTool extends Module {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}

50d012592f71b42c3531887d6f470862337b67ea
yjx0003 <yipengji@gmail.com>
2019-06-01 16:23:12 +0200

    Creado una clase para la internacionalizacion del texto

---
+++ src/controllers/I18n.java
@@ -0,0 +1,45 @@
+package controllers;
+
+import java.util.ResourceBundle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import model.Component;
+import model.Event;
+
+public class I18n {
+
+	static final Logger logger = LoggerFactory.getLogger(LoginController.class);
+	private static ResourceBundle rb;
+
+	public static void setResourceBundle(ResourceBundle rb) {
+		I18n.rb = rb;
+	}
+
+	public static ResourceBundle getResourceBundle() {
+		return rb;
+	}
+
+	public static String get(String key) {
+
+		return getOrDefault(key, key);
+	}
+
+	public static String get(Component component) {
+
+		return getOrDefault("component." + component, component.toString());
+	}
+
+	public static String get(Event event) {
+		return getOrDefault("eventname." + event, event.toString());
+	}
+
+	private static String getOrDefault(String key, String defaultValue) {
+		if (rb.containsKey(key)) {
+			return rb.getString(key);
+		}
+		logger.error("No existe entrada en el resource bundle la key: " + key);
+		return defaultValue;
+	}
+}

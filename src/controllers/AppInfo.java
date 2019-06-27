6a94b3693f41250d123c957673098a35c5d344ac
yjx0003 <yipengji@gmail.com>
2019-06-27 15:18:20 +0200

    Versión 2.6.3.5

--- src/controllers/AppInfo.java
+++ src/controllers/AppInfo.java
@@ -9,7 +9,7 @@ public class AppInfo {
 
 	public static final String APPLICATION_NAME = "UBUMonitor";
 
-	public static final String APPLICATION_VERSION = "v2.3.6.4";
+	public static final String APPLICATION_VERSION = "v2.3.6.5";
 
 	public static final String GITHUB = "https://github.com/yjx0003/UBUMonitor";
 

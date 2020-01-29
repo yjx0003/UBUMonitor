051d0d1644401949f1a85ab140da1354b2eec3f2
yjx0003 <yipeng996@gmail.com>
2020-01-29 12:59:08 +0100

    Change version to 2.6.0-dev

--- src/controllers/AppInfo.java
+++ src/controllers/AppInfo.java
@@ -8,7 +8,7 @@ package controllers;
  */
 public class AppInfo {
 
-	public static final String APPLICATION_VERSION = "v2.5.1-dev";
+	public static final String APPLICATION_VERSION = "v2.6.0-dev";
 
 	public static final String APPLICATION_NAME = "UBUMonitor";
 

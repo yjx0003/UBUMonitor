0a5d29b1b77633bdec6f73012ffbaf0f92ae71bc
xjx1001 <xjx1001@alu.ubu.es>
2020-02-07 17:48:03 +0100

    Implementado los algoritmos de clustering de apache #4

---
+++ src/clustering/algorithm/AlgorithmParameters.java
@@ -0,0 +1,87 @@
+package clustering.algorithm;
+
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.controlsfx.control.PropertySheet;
+
+import controllers.I18n;
+import javafx.beans.value.ObservableValue;
+
+public class AlgorithmParameters {
+
+	private Map<String, PropertySheet.Item> parameters;
+
+	public AlgorithmParameters() {
+		parameters = new LinkedHashMap<>();
+	}
+
+	public void addParameter(String name, Object value, String tooltip) {
+		ParameterItem c = new ParameterItem(name, value, tooltip);
+		parameters.put(name, c);
+	}
+
+	public Collection<PropertySheet.Item> getPropertyItems() {
+		return parameters.values();
+	}
+
+	@SuppressWarnings("unchecked")
+	public <V> V getValue(String name) {
+		return (V) parameters.get(name).getValue();
+	}
+
+	private static class ParameterItem implements PropertySheet.Item {
+
+		private String name;
+		private Object value;
+		private String tooltip;
+
+		public ParameterItem(String name, Object value, String tooltip) {
+			this.name = name;
+			this.value = value;
+			this.tooltip = tooltip;
+		}
+
+		@Override
+		public String getCategory() {
+			return null;
+		}
+
+		@Override
+		public String getName() {
+			return I18n.get(name);
+		}
+
+		@Override
+		public String getDescription() {
+			return I18n.get(tooltip);
+		}
+
+		@Override
+		public Object getValue() {
+			return value;
+		}
+
+		@Override
+		public void setValue(Object value) {
+			this.value = value;
+		}
+
+		@Override
+		public Optional<ObservableValue<? extends Object>> getObservableValue() {
+			return Optional.empty();
+		}
+
+		@Override
+		public Class<?> getType() {
+			return value.getClass();
+		}
+
+		@Override
+		public String toString() {
+			return name + "=" + value;
+		}
+	}
+}
---
+++ src/clustering/algorithm/FuzzyKMeans.java
@@ -0,0 +1,29 @@
+package clustering.algorithm;
+
+import org.apache.commons.math3.ml.clustering.Clusterable;
+import org.apache.commons.math3.ml.clustering.Clusterer;
+import org.apache.commons.math3.ml.clustering.FuzzyKMeansClusterer;
+import org.apache.commons.math3.ml.distance.DistanceMeasure;
+
+public class FuzzyKMeans extends Algorithm {
+
+	public static final String NAME = "FuzzyKMeans";
+
+	public FuzzyKMeans() {
+		super(NAME);
+		addParameter("clustering.numberOfClusters", 4);
+		addParameter("clustering.fuzziness", 2);
+		addParameter("clustering.maxIterations", 10);
+		addParameter("clustering.distance", Algorithms.DISTANCES_LIST.get(0));
+	}
+
+	@Override
+	public <T extends Clusterable> Clusterer<T> getClusterer() {
+		int k = getParameters().getValue("clustering.numberOfClusters");
+		int fuzziness = getParameters().getValue("clustering.fuzziness");
+		int max = getParameters().getValue("clustering.maxIterations");
+		DistanceMeasure distance = getParameters().getValue("clustering.distance");
+
+		return new FuzzyKMeansClusterer<>(k, fuzziness, max, distance);
+	}
+}

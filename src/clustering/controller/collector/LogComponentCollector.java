9cad3cbadfbb049549aeade10a11e14a51148b70
xjx1001 <xjx1001@alu.ubu.es>
2020-02-19 17:10:11 +0100

    Refactorización de las clases collector de logs

--- src/clustering/controller/collector/LogComponentCollector.java
+++ src/clustering/controller/collector/LogComponentCollector.java
@@ -1,45 +1,16 @@
 package clustering.controller.collector;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import clustering.data.UserData;
-import controllers.Controller;
 import controllers.I18n;
 import controllers.MainController;
 import controllers.datasets.DataSetComponent;
-import controllers.ubulogs.GroupByAbstract;
-import controllers.ubulogs.TypeTimes;
-import javafx.scene.control.ListView;
 import model.Component;
-import model.Course;
-import model.EnrolledUser;
 
-public class LogComponentCollector extends DataCollector {
-
-	private ListView<Component> components;
+public class LogComponentCollector extends LogCollector<Component> {
 
 	public LogComponentCollector(MainController mainController) {
-		components = mainController.getListViewComponents();
+		super(mainController.getListViewComponents(), DataSetComponent.getInstance());
 	}
 
-	@Override
-	public void collect(List<UserData> users) {
-		List<Component> selected = components.getSelectionModel().getSelectedItems();
-		DataSetComponent dataSet = DataSetComponent.getInstance();
-		Course couse = Controller.getInstance().getActualCourse();
-		GroupByAbstract<?> groupBy = couse.getLogStats().getByType(TypeTimes.ALL);
-		List<EnrolledUser> enrolledUsers = users.stream().map(UserData::getEnrolledUser).collect(Collectors.toList());
-		Map<EnrolledUser, Map<Component, List<Long>>> result = dataSet.getUserCounts(groupBy, enrolledUsers, selected, null, null);
-		for (UserData userData : users) {
-			for (Component component : selected) {
-				double datum = result.get(userData.getEnrolledUser()).get(component).get(0);
-				userData.addDatum(component.getName(), datum);
-			}
-		}
-	}
-	
 	@Override
 	public String toString() {
 		return I18n.get("tab.component");

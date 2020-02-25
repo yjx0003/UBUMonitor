e08a5b9be52935782f8dab456c6a51038da45a61
Yi Peng <yipeng996@gmail.com>
2020-02-25 13:28:36 +0100

    Added test

--- src/test/java/es/ubu/lsi/ubumonitor/WebServiceTest.java
+++ src/test/java/es/ubu/lsi/ubumonitor/WebServiceTest.java
@@ -1,11 +1,12 @@
 package es.ubu.lsi.ubumonitor;
 
-
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -14,10 +15,18 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 import es.ubu.lsi.ubumonitor.controllers.AppInfo;
 import es.ubu.lsi.ubumonitor.controllers.Controller;
+import es.ubu.lsi.ubumonitor.controllers.ubugrades.CreatorGradeItems;
 import es.ubu.lsi.ubumonitor.controllers.ubugrades.CreatorUBUGradesController;
+import es.ubu.lsi.ubumonitor.controllers.ubulogs.DownloadLogController;
+import es.ubu.lsi.ubumonitor.controllers.ubulogs.LogCreator;
 import es.ubu.lsi.ubumonitor.model.Course;
+import es.ubu.lsi.ubumonitor.model.CourseModule;
 import es.ubu.lsi.ubumonitor.model.DataBase;
+import es.ubu.lsi.ubumonitor.model.EnrolledUser;
+import es.ubu.lsi.ubumonitor.model.GradeItem;
+import es.ubu.lsi.ubumonitor.model.Logs;
 import es.ubu.lsi.ubumonitor.model.MoodleUser;
+import okhttp3.Response;
 
 @TestMethodOrder(OrderAnnotation.class)
 public class WebServiceTest {
@@ -25,12 +34,15 @@ public class WebServiceTest {
 	private static final String USERNAME = "teacher";
 	private static final String PASSWORD = "moodle";
 	private static final String HOST = "https://school.moodledemo.net";
-	private static final Controller CONTROLLER = Controller.getInstance();
+	private static final int COURSE_ID = 62;
+
+	private static Controller CONTROLLER;
 
 	@Test
 	@Order(1)
 	public void loginTest() throws IOException {
-		System.setProperty("logfile.name", ".log/"+AppInfo.APPLICATION_NAME_WITH_VERSION+"_Test.log");
+		System.setProperty("logfile.name", "./log/" + AppInfo.APPLICATION_NAME_WITH_VERSION + "-test.log");
+		CONTROLLER = Controller.getInstance();
 		CONTROLLER.tryLogin(HOST, USERNAME, PASSWORD);
 		CONTROLLER.setDataBase(new DataBase());
 	}
@@ -47,7 +59,48 @@ public class WebServiceTest {
 	@Order(3)
 	public void getUserCourses() throws IOException {
 		List<Course> courses = CreatorUBUGradesController.getUserCourses(CONTROLLER.getUser().getId());
-		assertTrue(!courses.isEmpty());
+		assertFalse(courses.isEmpty());
+		assertTrue(courses.stream().anyMatch(c -> c.getId() == COURSE_ID), "User not enrolled in the course id: " + COURSE_ID);
+		CONTROLLER.setActualCourse(CONTROLLER.getDataBase().getCourses().getById(COURSE_ID));
+	}
+
+	@Test
+	@Order(4)
+	public void getEnrolledUsers() throws IOException {
+		List<EnrolledUser> enrolledUsers = CreatorUBUGradesController.createEnrolledUsers(COURSE_ID);
+		assertFalse(enrolledUsers.isEmpty());
+	}
+	
+	@Test
+	@Order(5)
+	public void getCourseModules() throws IOException {
+		List<CourseModule> courseModules = CreatorUBUGradesController.createSectionsAndModules(COURSE_ID);
+		assertFalse(courseModules.isEmpty());
+	}
+	
+	@Test
+	@Order(6)
+	public void getGradeItems() throws IOException {
+		CreatorGradeItems creatorGradeItems = new CreatorGradeItems(new Locale(CONTROLLER.getUser().getLang()));
+		List<GradeItem> gradeItems = creatorGradeItems.createGradeItems(COURSE_ID);
+		assertFalse(gradeItems.isEmpty());
+	}
+	@Test
+	@Order(6)
+	public void getActivityCompletion() throws IOException {
+		CreatorUBUGradesController.createActivitiesCompletionStatus(COURSE_ID, CONTROLLER.getActualCourse().getEnrolledUsers());
+	}
+	
+	@Test
+	@Order(7)
+	public void getLogs() throws IOException {
+		DownloadLogController downloadLogController = LogCreator.download();
+
+		Response response = downloadLogController.downloadLog();
+	
+		Logs logs = new Logs(downloadLogController.getServerTimeZone());
+		LogCreator.parserResponse(logs, response);
+		CONTROLLER.getActualCourse().setLogs(logs);
 	}
 
 }

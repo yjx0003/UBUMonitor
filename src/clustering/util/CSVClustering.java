850085c99b9b782d598b13f977d38c4b983b1d89
xjx1001 <xjx1001@alu.ubu.es>
2020-03-01 17:39:27 +0100

    Exportar la tabla de clustering #20

---
+++ src/clustering/util/CSVClustering.java
@@ -0,0 +1,49 @@
+package clustering.util;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.opencsv.CSVWriter;
+
+import clustering.data.ClusterWrapper;
+import clustering.data.UserData;
+import model.EnrolledUser;
+
+public class CSVClustering {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(CSVClustering.class);
+
+	private static final String FILENAME = "clusering.csv";
+
+	private static final String[] HEAD = new String[] { "UserId", "FullName", "Cluster" };
+
+	public static void export(List<ClusterWrapper> clusters, Path path) {
+		try (Writer writer = Files.newBufferedWriter(path.resolve(FILENAME), StandardCharsets.UTF_8);
+				CSVWriter csvWriter = new CSVWriter(writer)) {
+
+			csvWriter.writeNext(HEAD);
+			for (ClusterWrapper clusterWrapper : clusters) {
+				for (UserData userData : clusterWrapper) {
+					EnrolledUser enrolledUser = userData.getEnrolledUser();
+					csvWriter.writeNext(new String[] {
+							String.valueOf(enrolledUser.getId()),
+							enrolledUser.getFullName(),
+							clusterWrapper.getName()
+					});
+				}
+			}
+
+		} catch (IOException e) {
+			LOGGER.error("Error writing csv file: {}.csv", path);
+			throw new IllegalStateException("Error exporting CSV file" + path, e);
+		}
+	}
+
+}

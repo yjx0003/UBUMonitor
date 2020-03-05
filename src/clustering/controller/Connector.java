90a6b0de4de7deb4054c0d4766de5382a193cf85
xjx1001 <xjx1001@alu.ubu.es>
2020-03-05 23:33:51 +0100

    Exportar la gráfica #23

--- src/clustering/controller/Connector.java
+++ src/clustering/controller/Connector.java
@@ -1,18 +1,29 @@
 package clustering.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.xml.bind.DatatypeConverter;
 
 import clustering.data.ClusterWrapper;
 import clustering.data.UserData;
+import javafx.scene.web.WebEngine;
 
 public class Connector {
 
 	private ClusteringController clusteringController;
+	
+	private WebEngine webEngine;
 
 	private List<ClusterWrapper> clusters;
 
 	public Connector(ClusteringController controller) {
 		clusteringController = controller;
+		webEngine = controller.getWebEngine();
 	}
 
 	public void selectUser(int clusterIndex, int index) {
@@ -23,6 +34,13 @@ public class Connector {
 
 	public void setClusters(List<ClusterWrapper> clusters) {
 		this.clusters = clusters;
+	}
+	
+	public void export(File file) throws IOException {
+		String str = (String) webEngine.executeScript("exportGraphic()");
+		byte[] imgdata = DatatypeConverter.parseBase64Binary(str.substring(str.indexOf(',') + 1));
+		BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imgdata));
+		ImageIO.write(bufferedImage, "png", file);
 	}
 
 }

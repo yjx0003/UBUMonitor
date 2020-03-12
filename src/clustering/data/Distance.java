82a0c4aeb611367233316d6c45049385e7394863
xjx1001 <xjx1001@alu.ubu.es>
2020-03-12 10:21:44 +0100

    Clase Distance

---
+++ src/clustering/data/Distance.java
@@ -0,0 +1,28 @@
+package clustering.data;
+
+import org.apache.commons.math3.ml.distance.CanberraDistance;
+import org.apache.commons.math3.ml.distance.ChebyshevDistance;
+import org.apache.commons.math3.ml.distance.DistanceMeasure;
+import org.apache.commons.math3.ml.distance.EarthMoversDistance;
+import org.apache.commons.math3.ml.distance.EuclideanDistance;
+import org.apache.commons.math3.ml.distance.ManhattanDistance;
+
+public enum Distance {
+
+	MANHATTAN_DISTANCE(new ManhattanDistance()),
+	EUCLIDEAN_DISTANCE(new EuclideanDistance()),
+	CANBERRA_DISTANCE(new CanberraDistance()),
+	CHEBYSHEV_DISTANCE(new ChebyshevDistance()),
+	EARTHMOVERS_DISTANCE(new EarthMoversDistance());
+
+	private DistanceMeasure instance;
+
+	private Distance(DistanceMeasure instance) {
+		this.instance = instance;
+	}
+
+	public DistanceMeasure getInstance() {
+		return instance;
+	}
+
+}

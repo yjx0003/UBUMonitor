fb84b50ec4c45123377de6c13a06c3315c97a6cf
xjx1001 <xjx1001@alu.ubu.es>
2020-04-05 19:23:28 +0200

    Mantener histórico de etiquetas para el renombrado #41

---
+++ src/main/java/es/ubu/lsi/ubumonitor/clustering/util/TextFieldPropertyEditorFactory.java
@@ -0,0 +1,66 @@
+package es.ubu.lsi.ubumonitor.clustering.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.controlsfx.control.PropertySheet.Item;
+import org.controlsfx.control.textfield.AutoCompletionBinding;
+import org.controlsfx.control.textfield.TextFields;
+import org.controlsfx.property.editor.AbstractPropertyEditor;
+import org.controlsfx.property.editor.PropertyEditor;
+
+import es.ubu.lsi.ubumonitor.controllers.configuration.ConfigHelper;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+import javafx.util.Callback;
+
+public class TextFieldPropertyEditorFactory implements Callback<Item, PropertyEditor<?>> {
+
+	private List<Editor> editors = new ArrayList<>();
+
+	@Override
+	public PropertyEditor<?> call(Item param) {
+		TextField textField = new TextField();
+		List<String> suggestions = ConfigHelper.getArray("labels");
+		Editor editor = new Editor(param, textField, suggestions);
+		editors.add(editor);
+		return editor;
+	}
+
+	public void add(String string) {
+		for (Editor editor : editors) {
+			editor.addAutoCompletion(string);
+		}
+	}
+
+	private static class Editor extends AbstractPropertyEditor<String, TextField> {
+		
+		private AutoCompletionBinding<String> suggestions;
+		private List<String> list;
+
+		public Editor(Item property, TextField control, List<String> list) {
+			super(property, control);
+			this.suggestions = TextFields.bindAutoCompletion(getEditor(), list);
+			this.list = list;
+		}
+		
+		private void addAutoCompletion(String string) {
+			System.out.println("TextFieldPropertyEditorFactory.Editor.addAutoCompletion()");
+			list.add(string);
+			suggestions.dispose();
+			suggestions = TextFields.bindAutoCompletion(getEditor(), list);
+		}
+
+		@Override
+		public void setValue(String value) {
+			getEditor().setText(value);
+		}
+
+		@Override
+		protected ObservableValue<String> getObservableValue() {
+			return getEditor().textProperty();
+		}
+
+	}
+
+}
